91b1c30985c89ff81c42052c44059890c6c35beb
AdarshVp77 <adarshvjayan77@gmail.com>
2024-01-04 20:04:54 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,104 @@
+// // console.log("Hello");
+
+// // // variables
+// // a = 10; // non-declared variable
+
+// // var a = 10; // declared variable
+
+// // let person = {
+// //   name: "Adarsh",
+// //   age: 24,
+// // };
+
+// // console.log(person.name);
+
+// // var arr = [
+// //   { name: "Adarsh", age: 12 },
+// //   { name: "Akash", age: 19 },
+// // ];
+
+// // console.log(arr[1]);
+
+// // let x = 7;
+// // let x1 = ++x;
+
+// // console.log(x1);
+
+// // if (x == x1) {
+// //   console.log("equal");
+// // } else {
+// //   console.log("not equal");
+// // }
+
+// let x = 10;
+// let y = 11;
+
+// if (x > y) {
+//   console.log("x is greater than y");
+// } else if (x === y) {
+//   console.log("x and y are equal ");
+// } else {
+//   console.log("x is less than y");
+// }
+
+// function subVal(num1, num2) {
+//   var sub = num1 - num2;
+//   return sub;
+// }
+// var result = subVal(10, 5);
+// console.log("The result is " + result);
+
+// function mulVal(num1, num2) {
+//   var mul = num1 * num2;
+//   return mul;
+// }
+// var result = mulVal(10, 5);
+// console.log("The result is " + result);
+
+// function divVal(num1, num2) {
+//   var div = num1 / num2;
+//   return div;
+// }
+
+// var result = divVal(10, 5);
+// console.log("The result is " + result
+
+// for (let n = 0; n <= 10; n++) {
+//   console.log(n);
+// }
+
+// let days = ["Monday", "Tuesday", "wednesday", "Thrusday", "Friday"];
+
+// for (let i = 0; i < days.length; i++) {
+//   console.log(days[i]);
+// }
+
+// let person = {
+//   name: "Adarsh",
+//   age: 24,
+//   place: "TVM",
+// };
+
+// for (x in person) {
+//   console.log(person[x]);
+// }
+
+// let j = 0;
+// while (j < 10) {
+//   console.log(j + 1);
+//   j++;
+// }
+
+// let days = ["Monday", "Tuesday", "wednesday", "Thrusday", "Friday"];
+// let i = 0;
+// while (i < days.length) {
+//   console.log(days[i]);
+//   i++;
+// }
+
+let i = 1;
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
